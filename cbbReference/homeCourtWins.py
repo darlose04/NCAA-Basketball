7b6927b46bd3e73f40ec3a247d52e7fdc4cd993d
Zach Smith <zsmith126028@gmail.com>
2018-11-25 16:58:49 -0600

    add bs to parse table... table > df > datalist

--- cbbReference/homeCourtWins.py
+++ cbbReference/homeCourtWins.py
@@ -12,3 +12,19 @@ url = "https://www.sports-reference.com/cbb/seasons/2018-school-stats.html"
 driver = webdriver.Chrome()
 driver.implicitly_wait(30)
 driver.get(url)
+
+soup_level1 = BeautifulSoup(driver.page_source, 'lxml')
+
+datalist = [] # empty list for storing tables
+
+# grab the table on the page
+table = soup_level1.find_all('table')[0]
+
+# giving the html table to pandas to put in a dataframe
+df = pd.read_html(str(table), header=0)
+
+# store the dataframe in a list
+datalist.append(df[0])
+
+# end the browser session
+driver.quit()
