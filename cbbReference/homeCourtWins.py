992e2cff485a8b26d4cde515a8f84c48a72d3f54
Zach Smith <zsmith126028@gmail.com>
2018-11-25 16:35:12 -0600

    add selenium chrome driver

--- cbbReference/homeCourtWins.py
+++ cbbReference/homeCourtWins.py
@@ -5,3 +5,10 @@ import re
 import pandas as pd
 from tabulate import tabulate
 import os
+
+url = "https://www.sports-reference.com/cbb/seasons/2018-school-stats.html"
+
+# create a new chrome session
+driver = webdriver.Chrome()
+driver.implicitly_wait(30)
+driver.get(url)
