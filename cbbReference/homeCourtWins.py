792786992a11dabbe1ad90050c9e369f1143eeaf
Zach Smith <zsmith126028@gmail.com>
2018-11-25 23:43:49 -0600

    remove unnecessary libraries

--- cbbReference/homeCourtWins.py
+++ cbbReference/homeCourtWins.py
@@ -1,12 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import re
-import pandas as pd
-from tabulate import tabulate
-import os
-
-
 
 url = "https://www.sports-reference.com/cbb/seasons/2018-school-stats.html"
 
@@ -17,6 +11,7 @@ driver.get(url)
 
 # start with 2018 season
 seasons = 2018
+
 # x and y variables are for the driver click method. 
 x = 16
 y = 17
@@ -25,16 +20,8 @@ y = 17
 homeWins = []
 homeLosses = []
 
-
-
-'''
-Instead of using this while loop in this fashion, I could probably create while loop that 
-changes the season year in the URL. This may result in cleaner code, so it's something
-I'll need to look into. Possibly with Puppeteer and JS.
-'''
-
 # loop through seasons going back to the 03-04 season
-while seasons > 2000:
+while seasons > 2014:
   # use Beautiful soup to pull info from tables
   soup = BeautifulSoup(driver.page_source, 'html.parser')
 
@@ -45,10 +32,7 @@ while seasons > 2000:
 
   for row in table.find_all("td", {"data-stat": "losses_home"}):
     homeLosses.append(int(row.get_text()))
-    
-
-
-
+   
   # these if statements add leading zeroes to the x and y values once they go below 10 (with zfill()). 
   # this is necessary because of the link_text in the html
   if x < 10:
@@ -66,10 +50,6 @@ while seasons > 2000:
   x -= 1
   y = int(y)
   y -= 1
-
-
-
-
 
 # end the browser session
 driver.quit()
