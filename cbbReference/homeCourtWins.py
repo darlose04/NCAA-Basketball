ef90f5199dc0a5b8ae9451e044aa604710a92e4c
Zach Smith <zsmith126028@gmail.com>
2018-11-25 19:35:24 -0600

    remove code. going to have to rethink how I get the necessary data

--- cbbReference/homeCourtWins.py
+++ cbbReference/homeCourtWins.py
@@ -6,6 +6,8 @@ import pandas as pd
 from tabulate import tabulate
 import os
 
+
+
 url = "https://www.sports-reference.com/cbb/seasons/2018-school-stats.html"
 
 # create a new chrome session
@@ -13,7 +15,7 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(30)
 driver.get(url)
 
-soup_level1 = BeautifulSoup(driver.page_source, 'lxml')
+soup_level1 = BeautifulSoup(driver.page_source, 'html.parser')
 
 datalist = [] # empty list for storing tables
 
@@ -21,22 +23,12 @@ datalist = [] # empty list for storing tables
 table = soup_level1.find_all('table')
 
 # giving the html table to pandas to put in a dataframe
-df = pd.read_html(str(table), header=0)
+df = pd.read_html(str(table), header=[0,1])
 
 # store the dataframe in a list
-datalist.append(df[0])
+datalist.append(df)
 
 # end the browser session
 driver.quit()
 
-# combine the dataframes in the list into one big dataframe (only one in list for now)
-result = pd.concat([pd.DataFrame(datalist[i]) for i in range(len(datalist))], ignore_index=True)
 
-# convert the pandas dataframe into JSON
-json_records = result.to_json(orient='records')
-
-# pretty print to CLI with tabulate
-# converts to an ascii table
-print(tabulate(result, headers=["RK", "School", "G", "W", "L", "W-L%", "SRS", "SOS", "Conf-W", "Conf-L", "Home-W", "Home-L", "Away-W", "Away-L", "Points-Tm", "Points-Opp", "", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "FT", "FTA", "FT%", "ORB", "TRB", "AST", "STL", "BLK", "TOV", "PF"], tablefmt='simple'))
-
-# print(datalist)
